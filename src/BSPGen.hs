91bee70a3ab8170f2ecf5fd79ac00b0ca9b41d5d
Marijan Gradecak <gradecam@tcd.ie>
2016-11-08 00:02:07 +0000

    Map now displays containers, rooms and hallways between rooms

--- src/BSPGen.hs
+++ src/BSPGen.hs
@@ -1,30 +1,46 @@
 module BSPGen (Point,Container,Tree,
                container, tree, room,
-               splitContainer, drawContainer, drawTree, drawRooms, getLeafs) where
+               splitContainer, drawContainer, drawTree, drawRooms, getLeafs, genPaths, drawPaths) where
 
 import System.Random (randomRIO)
-import Graphics.Gloss (Point, Picture, translate, rectangleWire, rectangleSolid, green, greyN, color, pictures)
+import Graphics.Gloss (Point, Picture, translate, rectangleWire, rectangleSolid,
+                       green, greyN, color, pictures, line, scale, polygon)
 
+{-  Containers -}
+{- Used in defining an area for which a room can be created in-}
 data Container = Container Point Float Float
-  deriving Show
 
+--constructor
 container :: Point -> Float -> Float -> Container
 container = Container
 
+-- generate a picture of container boundaries
 drawContainer :: Container -> Picture
 drawContainer (Container (x,y) w h) = color green $ translate (x+(w/2)) (y+(h/2)) $ rectangleWire w h
 
+{- Trees -}
+{- Basis of the Binary Space Partition Algorithm -}
+{- Used to sub divide containers and provide a path between them -}
 data Tree a = Nil
             | Leaf (Tree a) a (Tree a)
             deriving Show
-
+-- constructor
 tree ::  a -> Tree a
 tree a = Leaf Nil a Nil
+
+-- helper functions
+getVal :: Tree a -> a
+getVal (Leaf _ v _ ) = v
+
+isNil :: Tree a -> Bool
+isNil Nil    = True
+isNil Leaf{} = False
 
 drawTree :: Tree Container -> [Picture]
 drawTree Nil = []
 drawTree (Leaf l x r) = [drawContainer x]++ drawTree l ++ drawTree r
 
+-- return a list of containers at the bottom of the tree
 getLeafs :: Tree a -> [a]
 getLeafs Nil = []
 getLeafs (Leaf Nil a Nil) = [a]
@@ -32,14 +48,16 @@ getLeafs (Leaf t1 _ t2) = left ++ right
   where left = getLeafs t1
         right = getLeafs t2
 
+-- split a container into a tree of containers
 splitContainer :: Container -> Int -> IO (Tree Container)
 splitContainer r iter | iter == 0 = return $ tree r
-                 | otherwise = do
-                     (split1, split2) <- randomSplit r
-                     left <- splitContainer split1 (iter -1)
-                     right <- splitContainer split2 (iter-1)
-                     return $ Leaf left r right
+                      | otherwise = do
+                          (split1, split2) <- randomSplit r
+                          left <- splitContainer split1 (iter -1)
+                          right <- splitContainer split2 (iter-1)
+                          return $ Leaf left r right
 
+-- randomly split a container horizontaly or vertically
 randomSplit :: Container -> IO (Container, Container)
 randomSplit r = do
   a <- randomRIO (0,1) :: IO Int
@@ -50,38 +68,37 @@ randomSplit r = do
 verticalSplit :: Container -> IO (Container, Container)
 verticalSplit r@(Container (x,y) w h) = do
   newW <- randomRIO (1, w)
-  --print $ "Old w: " ++ show w ++ "New w: " ++ show newW
   let r1 = container (x,y) newW h
-      r2 = container ((x+newW),y) (w - newW) h
+      r2 = container (x+newW,y) (w - newW) h
   checkWRatio r (r1, r2)
 
 horizontalSplit :: Container -> IO (Container, Container)
 horizontalSplit r@(Container (x,y) w h) = do
   newH <- randomRIO (1, h)
-  -- print $ "Old h: " ++ show h ++ "New h: " ++ show newH
   let r1 = container (x,y) w newH
-      r2 = container (x,(y+newH)) w (h-newH)
+      r2 = container (x,y+newH) w (h-newH)
   checkHRatio r (r1, r2)
 
 checkWRatio :: Container -> (Container,Container) -> IO (Container, Container)
-checkWRatio r rs@(Container _ w h , Container _ w1 h1) = 
+checkWRatio r rs@(Container _ w h , Container _ w1 h1) =
   let r1 = w / h
       r2 = w1 / h1
-  in if r1 < 0.5 || r2 < 0.45
+  in if r1 < 0.45 || r2 < 0.45
      then randomSplit r
      else return rs
 
 checkHRatio :: Container -> (Container,Container) -> IO (Container, Container)
-checkHRatio r rs@(Container _ w h , Container _ w1 h1) = 
+checkHRatio r rs@(Container _ w h , Container _ w1 h1) =
   let r1 = h / w
       r2 = h1 / w1
-  in if r1 < 0.3 || r2 < 0.45
+  in if r1 < 0.45 || r2 < 0.45
      then randomSplit r
      else return rs
 
 {- Rooms -}
 type Room = Container
 
+-- generate a room from a container by preforming some random padding addition and substitution
 room :: Container -> IO Room
 room (Container (x,y) w h) = do
   xPad <- randomRIO (0, w/3)
@@ -97,7 +114,29 @@ room (Container (x,y) w h) = do
 drawRoom :: Room -> Picture
 drawRoom (Container (x,y) w h) =  color (greyN 0.5) $ translate (x+(w/2)) (y+(h/2)) $ rectangleSolid w h
 
-drawRooms :: [Room] -> [Picture]
-drawRooms r = map drawRoom r
+drawRooms :: [Room] -> Picture
+drawRooms r = pictures $ map drawRoom r
 
- 
+{- Path between point a and point b and the width of the path -}
+data Path = Path Point Point Float
+  deriving Show
+
+path :: Point -> Point -> Float -> Path
+path = Path
+
+genPath :: Container -> Container -> Path
+genPath (Container (x1,y1) w h) (Container (x2,y2) w1 h1) = path (x1+(w/2), y1+(h/2)) (x2+(w1/2), y2+(h1/2)) 2
+
+genPaths :: Tree Container ->  [Path]
+genPaths (Leaf r _ l )
+  | isNil r = []
+  | isNil l = []
+  | otherwise = [genPath (getVal r) (getVal l)] ++ genPaths r ++ genPaths l
+
+drawPath :: Path -> Picture
+drawPath (Path (x1,y1) (x2,y2) w)
+  | x1 == x2 = color (greyN 0.5) $ polygon [(x1-w, y1), (x2-w,y2), (x2+w, y2), (x1+w, y1)]
+  | otherwise = color (greyN 0.5) $ polygon [(x1,y1-w), (x2, y2-w), (x2, y2+w), (x1, y1+w)]
+
+drawPaths :: [Path] -> Picture
+drawPaths p = pictures $ map drawPath p
--- src/Main.hs
+++ src/Main.hs
@@ -8,12 +8,10 @@ import Control.Monad (mapM)
 {-Main-}
 main :: IO ()
 main = do
-  let main_container = container (-350,-350) 700 700
-  s <- splitContainer main_container 4
-  --print $ show s
+  s <- splitContainer (container (-350,-350) 700 700) 10 -- generate a tree of containters
+  r <-  mapM room (getLeafs s)
   let containers = pictures $ drawTree s
-  rooms <-  mapM room (getLeafs s)
-  let roomPic = pictures $ drawRooms  rooms
-  display (InWindow "Map" (1000, 1000) (0, 0)) black $ pictures [containers, roomPic]
+      rooms      = drawRooms r
+      paths      = drawPaths $ genPaths s
+  display (InWindow "Map" (701, 701) (250, 0)) black $ pictures [containers, paths, rooms]
 
-  --render defaultWindow (tree (room (1.5) (1.5) 4 4))
